3acf388bc33e5551931249baf1eda2f2bc46de03
errerew <erlina31acc@gmail.com>
2025-06-12 15:18:53 +0700

    menambahkan case 1,2,3,4

--- queues.cpp
+++ queues.cpp
@@ -128,7 +128,22 @@ void display(){
                 {
                     case '1';
                     {
-
+                        q.insert();
+                        break;
+                    }
+                    case '2';
+                    {
+                        q.remove();
+                        break;
+                    }
+                    case '3':
+                    {
+                        q.display();
+                        break;
+                    }
+                    case '4':
+                    {
+                        return 0;
                     }
                 }
             
