b43e00e1ca280e49a329f89e6c59cb7db4758740
errerew <erlina31acc@gmail.com>
2025-06-12 14:57:44 +0700

    menambahkan perintah untuk mengecek apakah antrian hanya memiliki satu elemen

--- queues.cpp
+++ queues.cpp
@@ -50,6 +50,17 @@ voidremove()
         return;
     }
     cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+    //cek jika antrian hanya memiliki satu elemen
+    if (FRONT == REAR)
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+    else
+    {
+
+    }
 }
 
 }
